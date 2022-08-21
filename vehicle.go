6df39f88f985c3ad21a0bde31ebf7a536f1ffcea
vrbilgi <vrbilgi@gmail.com>
2022-08-21 12:59:41 +0530

    Defining interface, structs, and variables

--- vehicle.go
+++ vehicle.go
@@ -4,12 +4,45 @@ import (
 
 )
 
+type vechile interface{
+
+}
+
+type Car struct {
+	model string
+	make string 
+	typeVechile string 
+}
+type truck struct{
+	model string
+	make string 
+	typeVechile string 
+}
+type bike struct{
+	model string
+	make string 
+}
 
 // Values array for the feedback.json file
-
+type values struct {
+	Models []Model `json:"values"`
+}
 // Model array for the feedback.json file
+type Model struct {
+	Name string `json:"model"`
+	Feedback []string `json:"feedback"`
+}
 
+type feedbackResult struct {
+	feedbackTotal int,
+	feedbackPositive int,
+	feedbackNegative int,
+	feedbackNeutral int
+}
 type rating float32
+
+var vehicleResult  map[string]feedbackResult
+var inventory  []vehicle
 
 const (
 	extraPositive rating = 1.2
@@ -21,7 +54,7 @@ const (
 
 func init() {
 
-/*
+
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
@@ -34,8 +67,8 @@ func init() {
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
-*/
-//	vehicleResult = make(map[string]feedbackResult)
+
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
