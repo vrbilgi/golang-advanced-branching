7d882b7201437c290c2571a6a8b2083ec5e15100
vrbilgi <vrbilgi@gmail.com>
2022-08-21 16:41:15 +0530

    Show vehicle ratings

--- vehicle.go
+++ vehicle.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -75,14 +76,7 @@ func init() {
 
 }
 
-func main() {
-
-	// Generate ratings for the different vehicles
-	generateRating()
-	// Print ratings for the different vehicles
-}
-
-func readJSONFile() Values {
+func readJSONfile() Values {
 	jsonFile, err := os.Open("feedback.json")
 
 	if err != nil {
@@ -131,5 +125,51 @@ func generateRating() {
 			}
 		}
 		vehicleResult[v.Name] = vehResult
+	}
+}
+
+func (c *car) carDetails() {
+
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Car", c.make, c.model)
+	showRating(c.model)
+}
+func (b *bike) bikeDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Bike", b.make, b.model)
+	showRating(b.model)
+}
+func (t *truck) truckDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Truck", t.make, t.model)
+	showRating(t.model)
+}
+func showRating(model string) {
+	ratingFound := false
+	for m, r := range vehicleResult {
+
+		if m == model {
+			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v", r.feedbackTotal, r.feedbackPositive, r.feedbackNegative, r.feedbackNeutral)
+			ratingFound = true
+		}
+		if !ratingFound {
+			fmt.Printf("No rating for this vehicle")
+		}
+	}
+}
+
+func main() {
+
+	// Generate ratings for the different vehicles
+	generateRating()
+	// Print ratings for the different vehicles
+	for _, veh := range inventory {
+		switch v := veh.(type) {
+		case car:
+			v.carDetails()
+		// case bike:
+		// 	v.bikeDetails()
+		// case truck:
+		// 	v.truckDetails()
+		default:
+			fmt.Printf("Are you sure this Vehicle Type exists")
+		}
 	}
 }
